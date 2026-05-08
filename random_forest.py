ddbd9121be8d7ab32d11099260a5df034946617d
StrivingForGood <strivingforgood8@gmail.com>
2026-05-08 21:55:56 +0300

    refactor remove GridSearchCV, since it's not suitable for the requirements

--- random_forest.py
+++ random_forest.py
@@ -6,7 +6,6 @@
 
 from pandas import read_csv
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
 # --- read data ---
@@ -29,35 +28,35 @@ y_test  = read_csv("data/y_test.csv").to_numpy().ravel()
 # truncated for training time
 # 4 * 4 * 3 * 3 = 144
 
-params = {
+RANDOM_STATE = 42
+N_JOBS = -1
+
+params: dict = {
     "n_estimators": [1, 55, 100, 200],
     "max_depth": list(range(5, 36, 15)) + [None],
     "min_samples_leaf": list(range(1, 12, 5)),
     "max_features": ["sqrt", "log2", None],
-    "random_state": [42],
-    "n_jobs": [-1]
 }
+
 
 # --- tune model ---
 
-default_model = RandomForestClassifier(random_state=42)
+default_model = RandomForestClassifier(
+    random_state=RANDOM_STATE,
+    n_jobs=N_JOBS
+)
 default_model.fit(X_train, y_train)
 
 tuned_model = RandomForestClassifier()
 
 best_score = -1
-best_params = [1, 1, 1, None]
 
-# grid = GridSearchCV(
-#     RandomForestClassifier(),
-#     params,
-#     n_jobs=-1,
-# )
-
-# grid.fit(X_train, y_train)
-# tuned_model = grid.best_estimator_
-# best_score = tuned_model.score(X_val, y_val)
-# best_params = [tuned_model.n_estimators, tuned_model.max_depth, tuned_model.min_samples_leaf, tuned_model.max_features]
+best_params: dict = {
+    "n_estimators": 1,
+    "max_depth": 1,
+    "min_samples_leaf": 1,
+    "max_features": None
+}
 
 for chosen_n_estimators in params["n_estimators"]:
     for chosen_max_depth in params["max_depth"]:
@@ -68,8 +67,8 @@ for chosen_n_estimators in params["n_estimators"]:
                     max_depth=chosen_max_depth,
                     min_samples_leaf=chosen_min_samples_leaf,
                     max_features=chosen_max_features,
-                    random_state=params["random_state"][0],
-                    n_jobs=params["n_jobs"][0]
+                    random_state=RANDOM_STATE,
+                    n_jobs=N_JOBS
                 )
                 tuned_model.fit(X_train, y_train)
 
@@ -80,15 +79,20 @@ for chosen_n_estimators in params["n_estimators"]:
 
                 if score > best_score:
                     best_score = score
-                    best_params = [chosen_n_estimators, chosen_max_depth, chosen_min_samples_leaf, chosen_max_features]
+                    best_params.update({
+                        "n_estimators": chosen_n_estimators,
+                        "max_depth": chosen_max_depth,
+                        "min_samples_leaf": chosen_min_samples_leaf,
+                        "max_features": chosen_max_features
+                    })
 
 tuned_model = RandomForestClassifier(
-    n_estimators=best_params[0],
-    max_depth=best_params[1],
-    min_samples_leaf=best_params[2],
-    max_features=best_params[3],
-    random_state=params["random_state"][0],
-    n_jobs=params["n_jobs"][0]
+    n_estimators=best_params["n_estimators"],
+    max_depth=best_params["max_depth"],
+    min_samples_leaf=best_params["min_samples_leaf"],
+    max_features=best_params["max_features"],
+    random_state=RANDOM_STATE,
+    n_jobs=N_JOBS
 )
 tuned_model.fit(X_train, y_train)
 
@@ -105,7 +109,7 @@ print(f"    Using parameters: n_estimators: {default_model.n_estimators}, max_de
 
 print("  Tuned model")
 print(f"    Best score: {tuned_validation_score*100:.2f}%")
-print(f"    Using parameters: n_estimators: {best_params[0]}, max_depth: {best_params[1]}, min_samples_leaf: {best_params[2]}, max_features: {best_params[3]}")
+print(f"    Using parameters: n_estimators: {best_params["n_estimators"]}, max_depth: {best_params["max_depth"]}, min_samples_leaf: {best_params["min_samples_leaf"]}, max_features: {best_params["max_features"]}")
 
 # --- test model ---
 
@@ -122,4 +126,4 @@ print(f"    Using parameters: n_estimators: {default_model.n_estimators}, max_de
 
 print("  Tuned model")
 print(f"    Best score: {tuned_test_score*100:.2f}%")
-print(f"    Using parameters: n_estimators: {best_params[0]}, max_depth: {best_params[1]}, min_samples_leaf: {best_params[2]}, max_features: {best_params[3]}")
+print(f"    Using parameters: n_estimators: {best_params["n_estimators"]}, max_depth: {best_params["max_depth"]}, min_samples_leaf: {best_params["min_samples_leaf"]}, max_features: {best_params["max_features"]}")
